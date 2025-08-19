4c8f954837e74a41c2ba26575d781b4ff690ed1d
CaixoDarksixo <jpnossol@gmail.com>
2025-08-19 11:29:14 -0300

    Update CategoriaE.java
    Troquei de ENUM para entidade

--- Backend/src/main/java/com/web2/manutencaoBackend/entity/CategoriaE.java
+++ Backend/src/main/java/com/web2/manutencaoBackend/entity/CategoriaE.java
@@ -1,5 +1,39 @@
 package com.web2.manutencaoBackend.entity;
 
-public enum CategoriaE {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class CategoriaE {
+	 @Id
+	 @GeneratedValue(strategy=GenerationType.IDENTITY)
+	 private Long id;
+	 private String nome;
+	 
+	 public CategoriaE() {
+		 
+	 }
+	 
+	 public CategoriaE(long id, String nome) {
+		 this.id = id;
+		 this.nome = nome;
+	 }
+
+	 public void setId(Long id) {
+		 this.id = id;
+	 }
+	 
+	 public Long getId() {
+		 return this.id;
+	 }
+	 
+	 public void setNome(String nome) {
+		 this.nome = nome;
+	 }
+	 
+	 public String getNome() {
+		 return this.nome;
+	 }
 }
