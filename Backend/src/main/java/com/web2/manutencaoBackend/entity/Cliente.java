8eed99a2b6513cedd5a9913fafb0c18a7661e6e4
CaixoDarksixo <jpnossol@gmail.com>
2025-08-19 11:26:29 -0300

    Update Cliente.java
    getters e setters

--- Backend/src/main/java/com/web2/manutencaoBackend/entity/Cliente.java
+++ Backend/src/main/java/com/web2/manutencaoBackend/entity/Cliente.java
@@ -10,7 +10,7 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String CPF;
+    private String cpf;
     private int telefone;
     private String nome, email, endereco;
 
@@ -19,10 +19,58 @@ public class Cliente {
 
 
     public Cliente(String CPF, int telefone, String nome, String email, String endereco) {
-        this.CPF = CPF;
+        this.cpf = CPF;
         this.telefone = telefone;
         this.nome = nome;
         this.email = email;
         this.endereco = endereco;
+    }
+    
+    public void setId(Long id) {
+    	this.id = id;
+    }
+    
+    public Long getId() {
+    	return this.id;
+    }
+    
+    public void setCpf(String cpf) {
+    	this.cpf = cpf;
+    }
+    
+    public String getCpf() {
+    	return this.cpf;
+    }
+    
+    public void setTelefone(int telefone) {
+    	this.telefone = telefone;
+    }
+    
+    public int getTelefone() {
+    	return this.telefone;
+    }
+    
+    public void setNome(String nome) {
+    	this.nome = nome;
+    }
+    
+    public String getNome() {
+    	return this.nome;
+    }
+    
+    public void setEmail(String email) {
+    	this.email = email;
+    }
+    
+    public String getEmail() {
+    	return email;
+    }
+    
+    public void setEndereco(String endereco) {
+    	this.endereco = endereco;
+    }
+    
+    public String getEndereco() {
+    	return this.endereco;
     }
 }
