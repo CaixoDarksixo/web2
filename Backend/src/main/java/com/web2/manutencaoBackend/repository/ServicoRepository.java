02f231a983d1e730aa23c9bcf52bbd52d5b5f264
Raí Boaventura <raiboaventura1@gmail.com>
2025-09-07 20:56:13 -0300

    criação metodos

--- Backend/src/main/java/com/web2/manutencaoBackend/repository/ServicoRepository.java
+++ Backend/src/main/java/com/web2/manutencaoBackend/repository/ServicoRepository.java
@@ -1,9 +1,24 @@
 package com.web2.manutencaoBackend.repository;
 
-import com.web2.manutencaoBackend.entity.Servico;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.web2.manutencaoBackend.entity.Servico;
+import com.web2.manutencaoBackend.entity.Status;
 
-public interface  ServicoRepository extends JpaRepository<Servico, Long>{
+public interface ServicoRepository extends JpaRepository<Servico, Long>{
+    
+    // Método para buscar todos os serviços de um cliente específico
+    List<Servico> findByIdCliente(Long idCliente);
+    
+    // Método para buscar serviços por status
+    List<Servico> findByStatus(Status status);
+    
+    // Método para buscar um serviço por ID e ID do cliente
+    Optional<Servico> findByIdAndIdCliente(Long id, Long idCliente);
+
+    // Método para buscar um serviço por ID e ID do funcionário
+    List<Servico> findByIdFuncionario(Long idFuncionario);
 
 }
--- Backend/src/main/java/com/web2/manutencaoBackend/repository/UserRepository.java
+++ Backend/src/main/java/com/web2/manutencaoBackend/repository/UserRepository.java
@@ -3,19 +3,9 @@ package com.web2.manutencaoBackend.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.web2.manutencaoBackend.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
-    // Declaração do método para buscar um usuário pelo login
-    Optional<User> findByLogin(String login);
-
-    private void register(User user){
-
-    };
-
-    private void update(User user){
-
-    };
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    Optional<User> findByEmail(String email);
 }
