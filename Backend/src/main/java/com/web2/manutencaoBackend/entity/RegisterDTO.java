c8a9f7a518b603b7c640b19f2da95c1315a9ee30
CaixoDarksixo <jpnossol@gmail.com>
2025-09-16 18:32:39 -0300

    Update RegisterDTO.java

--- Backend/src/main/java/com/web2/manutencaoBackend/entity/RegisterDTO.java
+++ Backend/src/main/java/com/web2/manutencaoBackend/entity/RegisterDTO.java
@@ -1,8 +1,10 @@
 package com.web2.manutencaoBackend.entity;
 
+import com.web2.manutencaoBackend.entity.UserRole;
+
 public class RegisterDTO(
     String login,
     String senha,
-    String role
+    UserRole role
 ){
 }
