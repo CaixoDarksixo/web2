b9b5163f3559e8578054c00995e65929f2cc4038
Raí Boaventura <147679342+RaiBoaventura@users.noreply.github.com>
2025-09-30 14:59:18 -0300

    teste

--- Backend/src/main/java/com/web2/manutencaoBackend/entity/RegisterDTO.java
+++ Backend/src/main/java/com/web2/manutencaoBackend/entity/RegisterDTO.java
@@ -1,7 +1,18 @@
 package com.web2.manutencaoBackend.entity;
 
-public record  RegisterDTO (
-    String login,
+import com.web2.manutencaoBackend.entity.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterDTO (
+    @NotBlank(message = "O e-mail é obrigatório.")
+    @Email(message = "O e-mail deve ser válido.")
+    String email,
+
+    @NotBlank(message = "A senha é obrigatória.")
+    @Size(min = 4, message = "A senha deve ter pelo menos 4 caracteres.")
     String senha,
+
     UserRole role
 ){ }
