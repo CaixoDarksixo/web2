9a40f6c6958de5b0e3e0e9a89e2007a9c5d9b5b9
João Leal <joaovictor.lg020@gmail.com>
2025-09-30 12:05:25 -0300

    refactor: update registration component

--- Frontend/src/app/pages/auth/auth.routes.ts
+++ Frontend/src/app/pages/auth/auth.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Access } from './access';
 import { Login } from './login';
 import { Error } from './error';
-import { Registration } from './registration';
+import { Registration } from './registration/registration';
 
 export default [
     { path: 'access', component: Access },
