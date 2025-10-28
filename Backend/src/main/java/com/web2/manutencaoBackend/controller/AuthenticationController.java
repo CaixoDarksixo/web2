95caf03561720c6795d5a65dcf7a84d6dbd56a27
GUINH0-0 <avancozambon@ufpr.br>
2025-10-28 18:21:26 -0300

    Mudança no autenticador

--- Backend/src/main/java/com/web2/manutencaoBackend/controller/AuthenticationController.java
+++ Backend/src/main/java/com/web2/manutencaoBackend/controller/AuthenticationController.java
@@ -1,17 +1,16 @@
 package com.web2.manutencaoBackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.web2.manutencaoBackend.entity.User;
-import com.web2.manutencaoBackend.infra.TokenService;
+import com.web2.manutencaoBackend.repository.UserRepository;
 
 import jakarta.validation.Valid;
 
@@ -19,7 +18,7 @@ import jakarta.validation.Valid;
 @RequestMapping("/auth")
 public class AuthenticationController {
 
-    @Autowired
+/*  @Autowired
     private AuthenticationManager authenticationManager;
 
     @Autowired
@@ -35,5 +34,32 @@ public class AuthenticationController {
         String token = tokenService.generateToken(user);
 
         return ResponseEntity.ok(new LoginResponseDTO(token));
+    } */
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid AuthenticationDTO request) {
+
+        User user = userRepository.findByLogin(request.getLogin())
+                .orElse(null);
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Usuário não encontrado");
+        }
+
+        boolean senhaCorreta = passwordEncoder.matches(request.getSenha(), user.getPassword());
+
+        if (!senhaCorreta) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Senha incorreta");
+        }
+
+        return ResponseEntity.ok("Login bem-sucedido!");
     }
 }
